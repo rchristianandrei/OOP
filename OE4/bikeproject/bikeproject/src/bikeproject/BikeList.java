1fe5dba4e6d985196380355112abc2d29244f906
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-06-10 17:07:43 +0800

    mixed void methods

--- OE4/bikeproject/bikeproject/src/bikeproject/BikeList.java
+++ OE4/bikeproject/bikeproject/src/bikeproject/BikeList.java
@@ -1,6 +1,7 @@
 package bikeproject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class BikeList {
 	
@@ -11,16 +12,28 @@ public class BikeList {
 		
 		fillArray(bikeList);
 		displayStock(bikeList);
-		
+		System.out.println();
 		displayBikeNumbers(bikeList);
 	}
 	
 	static void fillArray(ArrayList<Bike> bikeList) {
+		Random random = new Random();
 		
+		for(int i = 0; i < 10; i++) {	
+			int choice = random.nextInt(0,2);
+			
+			switch(choice) {
+				case 0: bikeList.add(new MountainBike()); break;
+				case 1: bikeList.add(new RoadBike()); break;
+			}
+		}
 	}
 
 	static void displayStock(ArrayList<Bike> bikeList) {
 		
+		for(Bike bike : bikeList) {
+			bike.printDescription();
+		}
 	}
 	
 	static int calculateStock(ArrayList<Bike> bikeList) {
